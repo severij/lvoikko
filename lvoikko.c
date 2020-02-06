0474e93cd0253908a7b9be3d2e766cba10c3cca8
Severi Jääskeläinen <severij@protonmail.com>
2020-02-06 20:48:41 +0200

    Removed options related stuff + few unnecessaries

--- lvoikko.c
+++ lvoikko.c
@@ -10,83 +10,6 @@
 #include <lauxlib.h>
 #include <libvoikko/voikko.h>
 
-#define MT_NAME "VOIKKO_HANDLE"
-#define HANDLE_REGISTRY_KEY "VOIKKO_HANDLE_REGISTRY_KEY"
-#define N_OPTIONS 16
-#define MAX_N_SUGGESTIONS 100
-
-/* typedef struct opt_info { */
-/* 	const int opt_id; */
-/* 	const int opt_type; */
-/* } opt_info_t; */
-
-/* typedef struct opt_map { */
-/* 	const char* opt_str; */
-/* 	const opt_info_t opt_info; */
-/* } opt_map_t; */
-
-/* static const opt_map_t options[] = { */
-/* 	{"accept_all_uppercase", {VOIKKO_OPT_ACCEPT_ALL_UPPERCASE, 0}}, */
-/* 	{"accept_bulleted_lists", {VOIKKO_OPT_ACCEPT_BULLETED_LISTS_IN_GC, 0}}, */
-/* 	{"accept_extra_hyphens", {VOIKKO_OPT_ACCEPT_EXTRA_HYPHENS, 0}}, */
-/* 	{"accept_first_uppercase", {VOIKKO_OPT_ACCEPT_FIRST_UPPERCASE, 0}}, */
-/* 	{"accept_missing_hyphens", {VOIKKO_OPT_ACCEPT_MISSING_HYPHENS, 0}}, */
-/* 	{"hyphenate_unknown_words", {VOIKKO_OPT_HYPHENATE_UNKNOWN_WORDS, 0}}, */
-/* 	{"ignore_dot", {VOIKKO_OPT_IGNORE_DOT, 0}}, */
-/* 	{"ignore_nonwords", {VOIKKO_OPT_IGNORE_NONWORDS, 0}}, */
-/* 	{"ignore_numbers", {VOIKKO_OPT_IGNORE_NUMBERS, 0}}, */
-/* 	{"ignore_uppercase", {VOIKKO_OPT_IGNORE_UPPERCASE, 0}}, */
-/* 	{"min_hyphenated_word_length", {VOIKKO_MIN_HYPHENATED_WORD_LENGTH, 1}}, */
-/* 	{"no_ugly_hyphenation", {VOIKKO_OPT_NO_UGLY_HYPHENATION, 0}}, */
-/* 	{"ocr_suggestions", {VOIKKO_OPT_OCR_SUGGESTIONS, 0}}, */
-/* 	{"speller_cache_size", {VOIKKO_SPELLER_CACHE_SIZE, 1}}, */
-/* }; */
-
-/* const opt_info_t *get_option_info(const char *opt_str) { */
-/* 	int i; */
-/* 	for (i=0; i < N_OPTIONS; i++) { */
-/* 		if (strcmp(opt_str, options[i].opt_str)) { */
-/* 			return &options[i].opt_info; */
-/* 		} */
-/* 	} */
-/* 	return NULL; */
-/* } */
-
-/* /1* int set_option(lua_State *L) { *1/ */
-
-/* 	/1* luaL_checkoption *1/ */
-
-/* 	struct VoikkoHandle *handle; */
-/* 	const char *key = lua_tostring(L, 1); */
-/* 	int value; */
-
-/* 	const opt_info_t *opt_info = get_option_info(key); */
-
-/* 	if (!opt_info) { */
-/* 		luaL_error(L, "TODO: Error"); */
-/* 		return 1; */
-/* 	} */
-
-/* 	handle = get_handle(L); */
-
-/* 	switch ((*opt_info).opt_type) { */
-/* 		case 0: */
-/* 			value = lua_toboolean(L, 3); */
-/* 			voikkoSetBooleanOption(handle, (*opt_info).opt_id, value); */
-/* 			break; */
-/* 		case 1: */
-/* 			value = lua_tointeger(L, 3); */
-/* 			voikkoSetIntegerOption(handle, (*opt_info).opt_id, value); */
-/* 			break; */
-/* 		default: */
-/* 			luaL_error(L, "TODO: Error"); */
-/* 			return 1; */
-/* 	} */
-	
-/* 	return 0; */
-/* } */
-
-
 struct VoikkoHandle *lvoikko_checkhandle(lua_State *L, int index) {
 	
 	/* Get handle reference id */
@@ -248,7 +171,6 @@ const luaL_Reg methods[] = {
 	{"hyphenate", hyphenate},
 	/* {"insert_hyphens", insert_hyphens}, */
 	{"suggest", suggest},
-	/* {"set_option", set_option}, */
 	{"spell", spell},
 #if LUA_VERSION_NUM < 502
 	{"terminate", terminate},
